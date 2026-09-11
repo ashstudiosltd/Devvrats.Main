8174933d00928e1a3f3956eb48f6597138de36bc
ashutoshkahlon <ashstudiosltd@gmail.com>
2026-09-11 21:43:57 +0530

    changes

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,6 +4,8 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
@@ -18,7 +20,11 @@ export async function createClient() {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, {
                 ...options,
-                ...(process.env.NODE_ENV === "production"
+
+                // Share Supabase auth cookies across:
+                // www.devvrats.in
+                // sabha.devvrats.in
+                ...(isProduction
                   ? {
                       domain: ".devvrats.in",
                       secure: true,
